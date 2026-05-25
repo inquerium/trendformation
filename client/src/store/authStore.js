bb7db33c43081e8bb753d85ac92fd65c4071a329
inquerium <team@inquerium.com>
2026-05-25 13:28:56 -0500

    Everything you need, all at once.

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -2,8 +2,8 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
